7e533cb996410b2a7fd64b101ea6f1d830144fcd
matiasmedina173 <maty_medina2002@outlook.com>
2024-02-17 21:15:26 -0300

    Proyecto Final React JS TERMINADO--Medina Otero Jose Matias

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,37 +1,39 @@
+import React, { useContext } from "react";
 import CartItem from "../CartItem/CartItem";
 import { Link } from "react-router-dom";
 import { CarritoContext } from "../../context/CarritoContext";
-import { useContext } from "react";
+import "./Cart.css"; // Importa el archivo CSS de estilos
 
 const Cart = () => {
-  const { carrito, vaciarCarrito, total, cantidadTotal } =
-    useContext(CarritoContext);
+  const { carrito, vaciarCarrito, total, cantidadTotal } = useContext(
+    CarritoContext
+  );
 
   if (cantidadTotal === 0) {
     return (
-      <>
+      <div className="cart-empty">
         <h2>No hay productos en el carrito</h2>
         <Link className="miBtn" to="/">
-          {" "}
-          Ver Productos{" "}
+          Ver Productos
         </Link>
-      </>
+      </div>
     );
   }
+
   return (
-    <div>
+    <div className="cart-container">
       {carrito.map((prod) => (
         <CartItem key={prod.id} {...prod} />
       ))}
-      <h3> Total:$ {total} </h3>
-      <button className="miBtn" onClick={() => vaciarCarrito()}>
-        {" "}
-        Vaciar Carrito{" "}
-      </button>
-      <Link className="miBtn" to="/checkout">
-        {" "}
-        Finalizar Compra{" "}
-      </Link>
+      <h3 className="cart-total"> Total:$ {total} </h3>
+      <div className="cart-buttons">
+        <button className="miBtn" onClick={() => vaciarCarrito()}>
+          Vaciar Carrito
+        </button>
+        <Link className="miBtn" to="/checkout">
+          Finalizar Compra
+        </Link>
+      </div>
     </div>
   );
 };
